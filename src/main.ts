0b1a832e64216760d8df2419247a2b1a470ea95d
taning <taninsist@qq.com>
2025-01-05 22:33:22 +0800

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  // 从环境变量获取 CORS 配置
+  const corsOrigins =
+    configService.get<string>('CORS_ORIGINS')?.split(',') || [];
+  const corsMethods =
+    configService.get<string>('CORS_METHODS')?.split(',') || [];
+  const corsHeaders =
+    configService.get<string>('CORS_HEADERS')?.split(',') || [];
+
+  app.enableCors({
+    origin: corsOrigins,
+    methods: corsMethods,
+    credentials: true,
+    allowedHeaders: corsHeaders,
+    exposedHeaders: corsHeaders,
+  });
+
+  const BASE_API = configService.get<string>('BASE_API');
+
+  app.setGlobalPrefix(BASE_API || 'api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
