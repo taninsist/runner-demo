4862eb00daf53a99844bbc61e0591551d22a4973
taning <taninsist@qq.com>
2025-01-08 08:19:43 +0800

    asf

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,6 +7,9 @@ export class AppService {
       baseApi: process.env.BASE_API,
       port: process.env.PORT,
       nodeEnv: process.env.NODE_ENV,
+      corsOrigins: process.env.CORS_ORIGINS,
+      corsMethods: process.env.CORS_METHODS,
+      corsHeaders: process.env.CORS_HEADERS,
     });
   }
 }
