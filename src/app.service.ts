d559cb138c8237b3069947680360ebb50d854d68
taning <taninsist@qq.com>
2025-01-19 22:19:02 +0800

    safd

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,12 +4,13 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     return JSON.stringify({
-      baseApi: process.env.BASE_API + 1,
-      port: process.env.PORT + 2,
-      nodeEnv: process.env.NODE_ENV + 3,
-      corsOrigins: process.env.CORS_ORIGINS || '123',
-      corsMethods: process.env.CORS_METHODS || '123',
-      corsHeaders: process.env.CORS_HEADERS || '123',
+      a: 123,
+      // baseApi: process.env.BASE_API + 1,
+      // port: process.env.PORT + 2,
+      // nodeEnv: process.env.NODE_ENV + 3,
+      // corsOrigins: process.env.CORS_ORIGINS || '123',
+      // corsMethods: process.env.CORS_METHODS || '123',
+      // corsHeaders: process.env.CORS_HEADERS || '123',
     });
   }
 }
